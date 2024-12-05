35b71658ddcadf664008c59a8d32e699a0e86023
Alan Meigs <drakosfire@gmail.com>
2024-12-04 20:26:04 -0700

    added script for build:dev argument to force the .env.development variables

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
+// config.ts
+// This file is used to set the configuration for the application.
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('REACT_APP_DUNGEONMIND_API_URL:', process.env.REACT_APP_DUNGEONMIND_API_URL);
 export const DUNGEONMIND_API_URL = process.env.REACT_APP_DUNGEONMIND_API_URL || 'https://dev.dungeonmind.net';
-console.log(DUNGEONMIND_API_URL);
+console.log('Final API URL:', DUNGEONMIND_API_URL);
 
