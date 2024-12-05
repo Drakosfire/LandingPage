9c0f23c62edd747ecf7110bfbbfc2ac1607abab9
Alan Meigs <drakosfire@gmail.com>
2024-12-04 20:26:45 -0700

    clicking ruleslawyer opens a new tab

--- src/components/AppLinks.tsx
+++ src/components/AppLinks.tsx
@@ -5,24 +5,25 @@ import { DUNGEONMIND_API_URL } from '../config';
 import { Link } from 'react-router-dom';
 
 // environment variable to store the route
-const route = `${DUNGEONMIND_API_URL}/storegenerator`;
+const storeGeneratorRoute = `${DUNGEONMIND_API_URL}/storegenerator`;
+const rulesLawyerRoute = `${DUNGEONMIND_API_URL}/ruleslawyer`;
 const AppLinks: React.FC = () => {
     return (
         <div className="app-links">
-            <a href={route} className="app-card" target="_blank" rel="noopener noreferrer">
+            <a href={storeGeneratorRoute} className="app-card" target="_blank" rel="noopener noreferrer">
                 <img
                     src={`${process.env.PUBLIC_URL}/images/StoreGeneratorLogov2.png`}
                     alt="Store Generator"
                     className="app-logo"
                 />
             </a>
-            <Link to="/ruleslawyer" className="app-card">
+            <a href={rulesLawyerRoute} className="app-card" target="_blank" rel="noopener noreferrer">
                 <img
                     src="https://imagedelivery.net/SahcvrNe_-ej4lTB6vsAZA/0ed83976-6007-4b56-7943-1c08d3117e00/public"
                     alt="Rules Lawyer"
                     className="app-logo"
                 />
-            </Link>
+            </a>
             <a href="/statblockgenerator" className="app-card" target="_blank" rel="noopener noreferrer">
                 <img
                     src="https://imagedelivery.net/SahcvrNe_-ej4lTB6vsAZA/99334c98-8fef-4c5c-bb18-1deaedb6da00/public"
