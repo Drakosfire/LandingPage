54d727edc641a602c75f290200b0281779edd351
Alan Meigs <drakosfire@gmail.com>
2024-12-28 10:56:55 -0700

    Corrected host address in config for local testing to port 7860

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 console.log('NODE_ENV:', process.env.NODE_ENV);
 console.log('REACT_APP_DUNGEONMIND_API_URL:', process.env.REACT_APP_DUNGEONMIND_API_URL);
 export const DUNGEONMIND_API_URL = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
+    ? 'http://localhost:7860'
     : 'https://dungeonmind.net';
 console.log('Final API URL:', DUNGEONMIND_API_URL);
 
